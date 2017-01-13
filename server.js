89a5471d7e30e7a2dcff20b685d4a626438ff650
NOEL S. DAVID <noelsdavid@gmail.com>
2017-01-13 15:26:21 +0000

    port number

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(8081);
+}).listen(process.env.PORT);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:' + process.env.PORT +'/');
